c94fd658df49f10a47ff5bf72e2f3ca88c3d5486
testgit <luuquangtri95@gmail.com>
2021-12-13 00:16:03 +0700

    done search

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
-import postApi from './apis/postApi'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import debounce from 'lodash.debounce'
+import postApi from './apis/postApi'
 import { getPaginationElement, setTextContent, setThumbnail, truncate } from './utils'
 
 dayjs.extend(relativeTime)
@@ -30,7 +31,7 @@ function createPostElement(post) {
 }
 
 function renderPostList(ulElementId, postList) {
-  if (!Array.isArray(postList) || postList.length === 0) return
+  if (!Array.isArray(postList)) return
 
   const ulElement = document.getElementById(ulElementId)
   if (!ulElement) return
@@ -68,6 +69,10 @@ async function handleFilterChange(filterName, filterValue) {
   try {
     const url = new URL(window.location)
     url.searchParams.set(filterName, filterValue)
+
+    //reset page if needed
+    if (filterName === 'title_like') url.searchParams.set('_page', 1)
+
     history.pushState({}, '', url)
 
     // fetch API
@@ -132,11 +137,30 @@ function initURL() {
   history.pushState({}, '', url)
 }
 
+function initSearchInput() {
+  const searchInput = document.getElementById('searchInput')
+  if (!searchInput) return
+
+  // update value to searchInput
+  const queryParams = new URLSearchParams(window.location.search)
+  if (queryParams.get('title_like')) searchInput.value = queryParams.get('title_like')
+
+  // set default value form query params
+  // title_like
+  const debounceSearch = debounce(
+    (event) => handleFilterChange('title_like', event.target.value),
+    500
+  )
+
+  searchInput.addEventListener('input', debounceSearch)
+}
+
 // MAIN
 ;(async () => {
   try {
     // attach event for links
     initPagination()
+    initSearchInput()
 
     // set default pagination (_page,_limit) on URL
     initURL()
