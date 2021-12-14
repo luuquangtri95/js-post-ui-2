2184071dd16fed09c528fa6e1580c9bfa381da8c
testgit <luuquangtri95@gmail.com>
2021-12-15 03:31:13 +0700

    done func get form value

--- js/utils/addEditPost/_initForm.js
+++ js/utils/addEditPost/_initForm.js
@@ -9,6 +9,23 @@ function setFormValue(form, formValue) {
   setBackgroundImage(document, '#postHeroImage', formValue?.imageUrl)
 }
 
+function getFormValue(form) {
+  const formValues = {}
+
+  // ;['title', 'author', 'description', 'imageUrl'].forEach((name) => {
+  //   const field = form.querySelector(`[name="${name}"]`)
+  //   if (field) value[name] = field.value
+  // })
+
+  // cach 2
+  const data = new FormData(form)
+  for (let [key, value] of data) {
+    formValues[key] = value
+  }
+
+  return formValues
+}
+
 export function initForm({ elementId, defaultValue, onSubmit }) {
   const formElement = document.getElementById(elementId)
   if (!formElement) return
@@ -17,5 +34,12 @@ export function initForm({ elementId, defaultValue, onSubmit }) {
 
   formElement.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    // get form value
+    const formValue = getFormValue(formElement)
+    console.log(formValue)
+    // validation
+    // if valid trigger submit callback
+    // otherwise, show validation error
   })
 }
